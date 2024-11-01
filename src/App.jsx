8e55fc033f5cc0a8b3f2e2c47cf8e993536737d6
jessewiegand21 <jessewiegand21@yahoo.com>
2024-10-31 18:39:38 -0600

    puppy pals

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,29 @@
 import './App.css'
 import {puppyList} from './data.js';
 import { useState } from 'react';
+
+
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
   console.log(puppies);
+  function handleClick(){}
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
   return (
     <>
     {
       puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>
+        return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
       })
     }
+    {featPupId && (<div>{featPupId}
+      <h2>{featuredPup.name}</h2>
+      <ul>
+        <li>Age: {featuredPup.age}</li>
+        <li>Email: {featuredPup.email}</li>
+      </ul>
+      </div>)}
     </>
   )
 }
